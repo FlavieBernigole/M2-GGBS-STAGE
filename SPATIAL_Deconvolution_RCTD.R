e357344e243bad7d6268faffae9248d0ffa36c4e
FlavieBernigole <bernigo.flavie@gmail.com>
2024-06-03 15:51:28 +0200

    SPATIAL_Deconvolution_RCTD.R

--- SPATIAL_Deconvolution_RCTD.R
+++ SPATIAL_Deconvolution_RCTD.R
@@ -1,3 +1,4 @@
+#Chargement des librairies
 library(Seurat)
 library(ggplot2)
 library(future)
@@ -13,7 +14,7 @@ library(scatterpie)
 library(ggcorrplot)
 
 
-
+#chargement de l'objet Seurat et des données de Spatial
 path<-"~/EP2C/10x_6573G/10x-D24-270-outs/"
 spatial.obj<-Load10X_Spatial(data.dir=path)
 spatial.obj <- subset(spatial.obj, subset = nCount_Spatial > 0)
@@ -22,14 +23,7 @@ spatial.obj$log_nCount_Spatial <- log(spatial.obj$nCount_Spatial)
 p2V <- SpatialFeaturePlot(spatial.obj, features = "log_nCount_Spatial") + theme(legend.position = "right")
 wrap_plots(p1V, p2V)
 
-
-#Noàrmalize by using sctransform, dimensionality reduction and clustering workflow
-spatial.obj <- SCTransform(spatial.obj, assay = "Spatial", ncells = 3000, verbose = FALSE)
-spatial.obj<- RunPCA(spatial.obj)
-spatial.obj <- RunUMAP(spatial.obj, dims = 1:30)
-spatial.obj <- FindNeighbors(spatial.obj, dims = 1:30)
-spatial.obj <- FindClusters(spatial.obj, resolution = 0.3, verbose = FALSE)
-#Visualisation
+#Visualisation des données de spatial
 p3V <- DimPlot(spatial.obj, reduction = "umap", label = TRUE)
 p4V <- SpatialDimPlot(spatial.obj, stroke = 0)
 p4V + p3V
@@ -55,14 +49,9 @@ spatial.obj$predicted.id <- GetTransferPredictions(spatial.obj)
 Idents(spatial.obj) <- "predicted.id"
 SpatialDimPlot(spatial.obj, cells.highlight = CellsByIdentities(object = spatial.obj, idents = c("Fibroblasts","Luminalcells","Macrophages")), facet.highlight = TRUE)
 
-
 #Identification d'entités spatialement variables
 DefaultAssay(spatial.obj) <- "SCT"
 spatial.obj <- FindSpatiallyVariableFeatures(spatial.obj, assay = "SCT", slot = "scale.data", features = VariableFeatures(spatial.obj)[1:1000],selection.method = "moransi", x.cuts = 100, y.cuts = 100)
-
-#Visualisation de l'expression des 6 principales caractéristiques identifiées par le I de Moran
-#SpatialFeaturePlot(spatial.obj, features = head(SpatiallyVariableFeatures(spatial.obj, selection.method = "moransi"),6), ncol = 3, alpha = c(0.1, 1), max.cutoff = "q95")
-#erreur d'ï¿½valuation de l'argument 'x' lors de la sï¿½lection d'une mï¿½thode pour la fonction 'head' : impossible d’utiliser xtfrm sur un tableau de données (data frame)
 
 #Déconvolution spatiale à l'aide d'un RCTD
 ref <- readRDS("~/Repertoire_travail_deconvolution/scRNAseqSCTvf5.rds")
